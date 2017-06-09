300f6f5c41e3e39c44fd7c14c779e21a75f38fb6
arthurjordao <arthurbernardijordao@gmail.com>
2017-06-08 21:29:04 -0300

    modify attribute to public

--- src/models/vase.ts
+++ src/models/vase.ts
@@ -2,6 +2,7 @@
  * Created by arthur on 08/06/17.
  */
 export class Vase {
-  constructor(public id: number, public humidity: number, public luminosity: number) {
+  constructor(public id: number, public humidity: number, public luminosity: number, public temperature: number,
+              public maxHumidity: number, public minHumidity: number, public minLuminosity: number) {
   }
 }
