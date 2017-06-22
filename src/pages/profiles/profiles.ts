0dc45d5a9468ff40735234f665b2298614672e2f
arthurjordao <arthurbernardijordao@gmail.com>
2017-06-22 12:57:23 -0300

    alert on delete

--- src/pages/profiles/profiles.ts
+++ src/pages/profiles/profiles.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, ModalController } from 'ionic-angular';
+import { NavController, ModalController, AlertController } from 'ionic-angular';
 import { FirebaseListObservable, AngularFireDatabase } from "angularfire2/database";
 import { EditProfilePage } from "../edit-profile/edit-profile";
 
@@ -20,7 +20,7 @@ export class ProfilesPage {
   showForm: boolean;
 
   constructor(public navCtrl: NavController, private afd: AngularFireDatabase,
-              public modal: ModalController) {
+              public modal: ModalController, private alertCtrl: AlertController) {
     this.profiles = this.afd.list('/profiles');
     this.newProfile = new Profile();
   }
@@ -33,6 +33,27 @@ export class ProfilesPage {
   
   editProfile(id: string) {
     this.modal.create(EditProfilePage, {id: id}).present();
+  }
+
+
+  confirmDelete(id: string, name: string){
+    let alert = this.alertCtrl.create({
+      title: 'Excluir perfil',
+      message: 'Você realmente deseja excluir o pefil: ' + name,
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        },
+        {
+          text: 'Deletar',
+          handler: () => {
+            this.deleteProfile(id)
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 
   deleteProfile(id: string){
